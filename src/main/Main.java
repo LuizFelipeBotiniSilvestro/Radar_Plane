0e6a5c45c492ef8c303d28e6a036e87966227f7e
LuizFelipeBotiniSilvestro <luizfelipebssideropolis@gmail.com>
2022-11-16 07:09:01 -0300

    commit inicial

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import telas.TMain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(new FlatDarkLaf());
+		} catch (Exception ex) {
+			System.err.println("Failed to initialize LaF");
+		}
+		new TMain().setVisible(true);
+	}
+}
